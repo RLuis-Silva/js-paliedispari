8ab0e745fa97b24c768d9a79f8fedd9629b0e41b
Luis Silva <s.ros.luis@gmail.com>
2020-10-22 15:41:35 +0200

    add function numeroRandom() + somma()

--- main.js
+++ main.js
@@ -8,13 +8,33 @@ var numGiocatore = parseInt(prompt("inserisci un numero da 1 a 5: "));
 
 if(numGiocatore >= 1 && numGiocatore <= 5){
     numeroValido = true;
-    console.log("Hai inserito un numero valido!");
+    console.log("Hai inserito un numero valido!: ", numGiocatore);
 }else{
     do{
-        numGiocatore = prompt("hai inserito un numero non valido, riprova!");
+        numGiocatore = parseInt(prompt("hai inserito un numero non valido, riprova!"));
         if(numGiocatore >= 1 && numGiocatore <= 5){
             numeroValido = true;
-            console.log("Ora hai inserito un numero valido!");
+            console.log("Ora hai inserito un numero valido!: ", numGiocatore);
         }
     }while(numeroValido == false);
+}
+
+
+if(numeroValido){ //numeroValido == true
+    //genera funziona numero random da 1 a 5
+    var numeroGenerato = numeroRandom();
+    console.log("il numero generato è:", numeroGenerato);
+    
+    // somma i due numeri
+    var numeroSommato = somma(numGiocatore, numeroGenerato);
+    console.log("la somma di " + numGiocatore + " + " + numeroGenerato + " = " + numeroSommato);
+}
+
+
+function numeroRandom(){
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+function somma(num1, num2){
+    return num1 + num2;
 }
