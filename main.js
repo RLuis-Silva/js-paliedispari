b5af74d950260bd953e736ba8e02bda3a68b4ba8
Luis Silva <s.ros.luis@gmail.com>
2020-10-23 10:17:09 +0200

    edit funzione numeroRandom() resa più riutilizzabile

--- main.js
+++ main.js
@@ -1,73 +1,75 @@
-console.log("js funziona!");
-// var rispostaGiocatore;
-// var numeroValido = false;
-// var stringValida = false;
+// console.log("js funziona!");
 
-// rispostaGiocatore = prompt("inserisci 'pari' o 'dispari: ").trim(); 
-// if(rispostaGiocatore == "pari" || rispostaGiocatore == "dispari"){
-//     stringValida = true;
-// }else{
-//     do{
-//         rispostaGiocatore = prompt("hai inserito un valore non valido. riprova. Digita 'pari' o 'dispari': ");
-//         if(rispostaGiocatore == "pari" || rispostaGiocatore == "dispari"){
-//             stringValida = true;
-//         }
-//     }while(stringValida == false);
-// }
-// console.log("hai inserito: ", rispostaGiocatore);
+var rispostaGiocatore;
+var numeroValido = false;
+var stringValida = false;
 
-// var numGiocatore = parseInt(prompt("inserisci un numero da 1 a 5: "));
+rispostaGiocatore = prompt("inserisci 'pari' o 'dispari: ").trim(); 
+if( (rispostaGiocatore == "pari") || (rispostaGiocatore == "dispari") ){
+    stringValida = true;
+}else{
+    do{
+        rispostaGiocatore = prompt("hai inserito un valore non valido. riprova. Digita 'pari' o 'dispari': ");
+        if(rispostaGiocatore == "pari" || rispostaGiocatore == "dispari"){
+            stringValida = true;
+        }
+    }while(stringValida == false);
+}
+console.log("hai inserito: ", rispostaGiocatore);
 
-// if(numGiocatore >= 1 && numGiocatore <= 5){
-//     numeroValido = true;
-//     console.log("Hai inserito un numero valido!: ", numGiocatore);
-// }else{
-//     do{
-//         numGiocatore = parseInt(prompt("hai inserito un numero non valido, riprova!"));
-//         if(numGiocatore >= 1 && numGiocatore <= 5){
-//             numeroValido = true;
-//             console.log("Ora hai inserito un numero valido!: ", numGiocatore);
-//         }
-//     }while(numeroValido == false);
-// }
+var numGiocatore = parseInt(prompt("inserisci un numero da 1 a 5: "));
+
+if( (numGiocatore >= 1) && (numGiocatore <= 5) ){
+    numeroValido = true;
+    console.log("Hai inserito un numero valido!: ", numGiocatore);
+}else{
+    do{
+        numGiocatore = parseInt(prompt("hai inserito un numero non valido, riprova!"));
+        if(numGiocatore >= 1 && numGiocatore <= 5){
+            numeroValido = true;
+            console.log("Ora hai inserito un numero valido!: ", numGiocatore);
+        }
+    }while(numeroValido == false);
+}
 
 
-// if(numeroValido){ //numeroValido == true
+if(numeroValido){ //numeroValido == true
     
-//     var numeroGenerato = numeroRandom();
-//     console.log("il numero generato è:", numeroGenerato);
+    //generare un  numero random da 1 a 5
+    var numeroGenerato = numeroRandom(1, 5);
+    console.log("il numero generato è:", numeroGenerato);
     
     
-//     var numeroSommato = somma(numGiocatore, numeroGenerato);
-//     console.log("la somma di " + numGiocatore + " + " + numeroGenerato + " = " + numeroSommato);
+    var numeroSommato = somma(numGiocatore, numeroGenerato);
+    console.log("la somma di " + numGiocatore + " + " + numeroGenerato + " = " + numeroSommato);
 
     
-//     var risultato = pariDispari(numeroSommato);
-//     if(risultato === rispostaGiocatore){
-//         console.log("Complimenti, hai vinto!");
-//     }else{
-//         console.log("Mi dispiace ma hai perso!");
-//     }
-// }
+    var risultato = pariDispari(numeroSommato);
+    if(risultato === rispostaGiocatore){
+        console.log("Complimenti, hai vinto!");
+    }else{
+        console.log("Mi dispiace ma hai perso!");
+    }
+}
 
-// //genera funziona numero random da 1 a 5
-// function numeroRandom(){
-//     return Math.floor(Math.random() * 5) + 1;
-// }
+//funzione che genera numero random da min a max
+function numeroRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
-// // somma i due numeri
-// function somma(num1, num2){
-//     return num1 + num2;
-// }
+// somma i due numeri
+function somma(num1, num2){
+    return num1 + num2;
+}
 
-// //stabilisce se la somma è pari o dispari
-// function pariDispari(num){
-//     if(num % 2 == 0){
-//         return "pari";
-//     }else{
-//         return "dispari";
-//     }
-// }
+//stabilisce se la somma è pari o dispari
+function pariDispari(num){
+    if(num % 2 == 0){
+        return "pari";
+    }else{
+        return "dispari";
+    }
+}
 
 
 
@@ -77,21 +79,21 @@ console.log("js funziona!");
 
 
 // esercizio1
-var stringaInput = prompt("inserisci una parola: ").trim();
-console.log(stringaInput);
+// var stringaInput = prompt("inserisci una parola: ").trim();
+// console.log(stringaInput);
 
-var nuovaStringa = stringaInvertita(stringaInput);
+// var nuovaStringa = stringaInvertita(stringaInput);
 
-if(stringaInput == nuovaStringa){
-    console.log("la stringa è palindroma");
-}else{
-    console.log("la stringa non è palindroma");
-}
+// if(stringaInput == nuovaStringa){
+//     console.log("la stringa è palindroma");
+// }else{
+//     console.log("la stringa non è palindroma");
+// }
 
-//inverte la stringa
-function stringaInvertita(stringa){
-    var newStringa = stringaInput.split("").reverse().join("");
-    console.log(newStringa);
-    return newStringa;
-}
+// //inverte la stringa
+// function stringaInvertita(stringa){
+//     var newStringa = stringaInput.split("").reverse().join("");
+//     console.log(newStringa);
+//     return newStringa;
+// }
 
